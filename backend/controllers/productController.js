33178938e9dac7cb114586f5e83d3736a01cd662
CHIRAGBHAYANA <chiragbhayana2k@gmail.com>
2022-04-11 13:52:21 +0530

    Complete CRUD Product Backend

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,7 +1,83 @@
-const getAllProducts = (req, res) => {
-  res.status(200).json({ message: "Route is Working fine" });
+const Product = require("../models/productModel");
+
+// Create Product -- Admin
+const createProduct = async (req, res, next) => {
+  const product = await Product.create(req.body);
+  res.status(201).json({
+    success: true,
+    product,
+  });
+};
+// Get all Product
+
+const getAllProducts = async (req, res) => {
+  const product = await Product.find();
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
+
+// Update Product -- Admin
+const updateProduct = async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
+
+// delete product -- Admin
+
+const deleteProduct = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+
+  await product.remove();
+
+  res.status(200).json({
+    success: true,
+    message: "Product Deleted Succesfully",
+  });
+};
+
+// get Single product details
+const getSingleProduct = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+  res.status(200).json({
+    success: true,
+    product,
+  });
 };
 
 module.exports = {
   getAllProducts,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  getSingleProduct,
 };
