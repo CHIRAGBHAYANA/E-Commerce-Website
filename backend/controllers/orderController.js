18a58d7e51140d3d45ff4b019e3cec2f66ce7238
CHIRAGBHAYANA <chiragbhayana2k@gmail.com>
2022-04-14 16:51:22 +0530

    Added getAllOrders,updateOrder,deleteOrder Routes Tested

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -60,8 +60,79 @@ const myOrders = catchAsyncErrors(async (req, res, next) => {
   });
 });
 
+// get all Orders -- Admin
+
+const getAllOrders = catchAsyncErrors(async (req, res, next) => {
+  const orders = await Order.find();
+
+  let totalAmount = 0;
+
+  orders.forEach((order) => {
+    totalAmount += order.totalPrice;
+  });
+
+  res.status(200).json({
+    success: true,
+    totalAmount,
+    orders,
+  });
+});
+
+// Update Order Status -- Admin
+const updateOrder = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+
+  if (!order) {
+    return next(new ErrorHandler("Order not found with this Id", 404));
+  }
+
+  if (order.orderStatus === "Delivered") {
+    return next(new ErrorHandler("You Have already delivered this order", 400));
+  }
+
+  order.orderItems.forEach(async (o) => {
+    await updateStock(o.product, o.quantity);
+  });
+
+  order.orderStatus = req.body.status;
+
+  if (req.body.status === "Delivered") {
+    order.deliveredAt = Date.now();
+  }
+
+  await order.save({ validateBeforeSave: false });
+  res.status(200).json({
+    success: true,
+  });
+});
+
+async function updateStock(id, quantity) {
+  const product = await Product.findById(id);
+  product.Stock -= quantity;
+  await product.save({ validateBeforeSave: false });
+}
+
+// Delete Order -- Admin
+
+const deleteOrder = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+
+  if (!order) {
+    return next(new ErrorHandler("Order not found with this Id", 404));
+  }
+
+  await order.remove();
+
+  res.status(200).json({
+    success: true,
+  });
+});
+
 module.exports = {
   newOrder,
   getSingleOrder,
   myOrders,
+  getAllOrders,
+  deleteOrder,
+  updateOrder,
 };
--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -3,6 +3,9 @@ const {
   newOrder,
   getSingleOrder,
   myOrders,
+  getAllOrders,
+  updateOrder,
+  deleteOrder,
 } = require("../controllers/orderController");
 const router = express.Router();
 
@@ -12,5 +15,12 @@ router.route("/order/new").post(isAuthenticatedUser, newOrder);
 
 router.route("/order/:id").get(isAuthenticatedUser, getSingleOrder);
 router.route("/orders/me").get(isAuthenticatedUser, myOrders);
+router
+  .route("/admin/orders")
+  .get(isAuthenticatedUser, authorizedRoles("admin"), getAllOrders);
+router
+  .route("/admin/order/:id")
+  .put(isAuthenticatedUser, authorizedRoles("admin"), updateOrder)
+  .delete(isAuthenticatedUser, authorizedRoles("admin"), deleteOrder);
 
 module.exports = router;
