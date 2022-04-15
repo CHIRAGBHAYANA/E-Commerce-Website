d9c68acf0691e40cff2c188b27e60f93d6e92795
CHIRAGBHAYANA <chiragbhayana2k@gmail.com>
2022-04-15 23:31:58 +0530

    Initialized React and Completed Navbar

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import Header from "./Component/Layout/Header.js";
+import { BrowserRouter as Router } from "react-router-dom";
+import WebFont from "webfontloader";
+import React from "react";
+
+function App() {
+  React.useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Roboto", "Droid Sans", "Chilanka"],
+      },
+    });
+  }, []);
+  return (
+    <Router>
+      <Header />
+    </Router>
+  );
+}
+
+export default App;
