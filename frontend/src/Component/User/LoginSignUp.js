c67eab88778acd7598f91d6095ee79e1607ad4e6
CHIRAGBHAYANA <chiragbhayana2k@gmail.com>
2022-04-27 10:59:24 +0530

    Add Register State in UI

--- frontend/src/Component/User/LoginSignUp.js
+++ frontend/src/Component/User/LoginSignUp.js
@@ -6,7 +6,7 @@ import MailOutlineIcon from "@material-ui/icons/MailOutline";
 import LockOpenIcon from "@material-ui/icons/LockOpen";
 import FaceIcon from "@material-ui/icons/Face";
 import { useDispatch, useSelector } from "react-redux";
-import { clearErrors, login } from "../../actions/userAction";
+import { clearErrors, login, register } from "../../actions/userAction";
 import { useAlert } from "react-alert";
 
 const LoginSignUp = ({ history }) => {
@@ -48,6 +48,8 @@ const LoginSignUp = ({ history }) => {
     myForm.set("email", email);
     myForm.set("password", password);
     myForm.set("avatar", avatar);
+
+    dispatch(register(myForm));
   };
 
   const registerDataChange = (e) => {
--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -3,6 +3,9 @@ import {
   LOGIN_FAIL,
   LOGIN_SUCCESS,
   CLEAR_ERRORS,
+  REGISTER_USER_FAIL,
+  REGISTER_USER_SUCCESS,
+  REGISTER_USER_REQUEST,
 } from "../constants/userConstants";
 
 import axios from "axios";
@@ -21,6 +24,26 @@ export const login = (email, password) => async (dispatch) => {
     dispatch({ type: LOGIN_SUCCESS, payload: data.user });
   } catch (error) {
     dispatch({ type: LOGIN_FAIL, payload: error.response.data.message });
+  }
+};
+
+export const register = (userData) => async (dispatch) => {
+  try {
+    dispatch({ type: REGISTER_USER_REQUEST });
+    const config = { headers: { "Content-Type": "multipart/form-data" } };
+
+    const { data } = await axios.post(
+      `/api/v1/register`,
+      { userData, config },
+      config
+    );
+
+    dispatch({ type: REGISTER_USER_SUCCESS, payload: data.user });
+  } catch (error) {
+    dispatch({
+      type: REGISTER_USER_FAIL,
+      payload: error.response.data.message,
+    });
   }
 };
 
